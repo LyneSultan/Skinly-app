f68355ab9b751d31c04e10d3548f50f45dd40583
LyneSultan <lynesultane@gmail.com>
2024-12-10 16:55:58 +0200

    created base style

---
+++ style/base.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const base = StyleSheet.create({
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '54',
+  },
+  wrap: {
+    flexWrap: 'wrap',
+  },
+  background: {
+    backgroundColor: 'black',
+  },
+});
