26b2962ea0c7a29f2016b998ae7ba83f020daa94
LyneSultan <lynesultane@gmail.com>
2024-12-16 01:33:55 +0200

    created input styling

---
+++ components/base/Input/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { colors } from './../../../colors/colors';
+
+export const styles = StyleSheet.create({
+  outline: {
+    borderWidth: 3,
+    borderRadius: 20,
+  },
+
+});
+export const theme= {
+  primary: colors.secondary,
+  outline: colors.secondary,
+  background: colors.background
+}
