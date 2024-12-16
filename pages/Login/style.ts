9d7888161a821ee607f6e78363b430f01f04c9c6
LyneSultan <lynesultane@gmail.com>
2024-12-16 23:29:13 +0200

    created styling for login page

---
+++ pages/Login/style.ts
@@ -0,0 +1,17 @@
+
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+
+  container: {
+    gap: 20,
+    width: "90%",
+  },
+  loginForm: {
+    gap:10
+  },
+  forgetPassword: {
+    alignSelf:"flex-end"
+  }
+
+})
