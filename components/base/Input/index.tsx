b1694e2e0bfb2ffcefce388a710764889d154c5f
LyneSultan <lynesultane@gmail.com>
2024-12-17 00:05:43 +0200

    added password input design to input component

--- components/base/Input/index.tsx
+++ components/base/Input/index.tsx
@@ -4,14 +4,17 @@ import { styles, theme } from './styles';
 interface inputProps {
   label: string;
   text: string;
+  password?: boolean
 }
-export const Input = ({ label, text }: inputProps) => {
+export const Input = ({ label, text, password }: inputProps) => {
 
   return (
     <TextInput
       label={label}
       placeholder={text}
-      value={""}
+      // value={""}
+      secureTextEntry={password}
+
       mode="outlined"
       outlineStyle={styles.outline}
       theme={{ colors: theme }}
