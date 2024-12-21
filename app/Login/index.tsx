5df910d1a8f0a54ff3f7febbf34e9c11a559d863
LyneSultan <lynesultane@gmail.com>
2024-12-21 12:31:15 +0200

    added color

--- app/Login/index.tsx
+++ app/Login/index.tsx
@@ -5,33 +5,37 @@ import { typography } from "@/style/typography"
 import { Link } from "expo-router"
 import { Image, Text, View } from "react-native"
 import { style } from "./style"
+import { useLoginLogic } from "./useLoginLogic"
 
 const Login = () => {
+  const { handleLogin } = useLoginLogic();
   return (
-    <View style={[style.container]}>
-      <Image source={require('@/assets/images/backIcon.png')} />
+    <View style={[base.flex, base.alignCenter]}>
+      <View style={[style.container]}>
+        <Image source={require('@/assets/images/backIcon.png')} />
 
-      <View>
-        <Text style={typography.h1}>Welcome back! Glad to see you, Again!</Text>
-      </View>
-
-      <View style={style.loginForm}>
-        <Input label="Name" text="Enter your name"></Input>
-        <Input label="password" text="Enter your password" password={true}></Input>
-      </View>
-
-      <View style={base.flex}>
-        <View style={style.forgetPassword}>
-          <Text>Forgot Password?</Text>
+        <View>
+          <Text style={typography.h1}>Welcome back! Glad to see you, Again!</Text>
         </View>
-      </View>
 
-      <ButtonComponent mode="contained" text="Login" />
+        <View style={style.loginForm}>
+          <Input label="Name" text="Enter your name"></Input>
+          <Input label="password" text="Enter your password" password={true}></Input>
+        </View>
+
+        <View style={base.flex}>
+          <View style={style.forgetPassword}>
+            <Text>Forgot Password?</Text>
+          </View>
+        </View>
+
+        <ButtonComponent mode="contained" text="Login" onPress={handleLogin} />
 
 
-      <View style={[base.flex, base.row, base.justifyCenter]}>
-        <Text>Don ‘t have an account? <Link href="/Register"><Text style={[style.login, typography.bold]}>Register</Text> </Link> </Text>
-      </View>
-    </View >)
+        <View style={[base.flex, base.row, base.justifyCenter]}>
+          <Text>Don ‘t have an account? <Link href="/Register"><Text style={[style.login, typography.bold]}>Register</Text> </Link> </Text>
+        </View>
+      </View >
+    </View>)
 }
 export default Login;
--- colors/colors.ts
+++ colors/colors.ts
@@ -2,5 +2,6 @@ export const colors = {
   primary: '#E5C1CD',
   secondary: '#CBDDD1',
   background: '#F0F0F0',
+  rose:'#F5E6E8'
 };
 
