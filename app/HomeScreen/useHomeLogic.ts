2c4cb6371ab7126305637863d70a074a6e3c1bcb
LyneSultan <lynesultane@gmail.com>
2024-12-21 20:50:53 +0200

    added gunction to handle view more

--- app/HomeScreen/useHomeLogic.ts
+++ app/HomeScreen/useHomeLogic.ts
@@ -1,18 +1,42 @@
 import { fetch } from 'expo/fetch';
-
-const fetchData = async () => {
-  try {
-    const response = await fetch('http://192.168.10.128:3000/product');
+import { useState } from 'react';
+export const useHomeLogic = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [page, setPage] = useState(1);
+ const fetchData = async () => {
+   try {
+    const response = await fetch('http://192.168.10.128:3000/product?page=1&pageSize=6');
     const data = await response.json();
     return data;
 
   } catch (error) {
     console.error(error);
   }
-};
-export default fetchData;
+  };
 
-export type Product = {
+  const handleViewMore=async(page:number) => {
+    try {
+      const response = await fetch(`http://192.168.10.128:3000/product?page=${page}&pageSize=6`);
+      const data = await response.json();
+      console.log(response);
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return {
+    fetchData,
+    handleViewMore,
+    data,
+    page,
+    setData,
+    setPage
+  }
+
+}
+export default useHomeLogic;
+ type Product = {
   image: string;
   title: string;
   price: string;
