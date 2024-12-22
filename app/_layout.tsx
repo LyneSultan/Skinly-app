f7dcda866eae831043adf9da535341e918c7aaea
LyneSultan <lynesultane@gmail.com>
2024-12-22 21:07:17 +0200

    updated root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+
+      <Stack.Screen name="index" options={{ title: 'Login' }} />
+      <Stack.Screen name="Register/index" options={{ title: 'Register' }} />
+      <Stack.Screen name="Login/index" options={{ title: 'Login' }} />
+      <Stack.Screen name="HomeScreen1/index" options={{ title: '1' }} />
+      <Stack.Screen name="SkinScan/index" options={{ title: 'Skin detection' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+    </Stack>
+  );
 }
