de13710ea5456b2a3e921b7b86cfe7a5623f33e4
LyneSultan <lynesultane@gmail.com>
2024-12-24 00:09:55 +0200

    added path prop to onBoarding card

--- components/OnBoardingCard/index.tsx
+++ components/OnBoardingCard/index.tsx
@@ -1,16 +1,22 @@
 import { typography } from "@/style/typography";
+import { useRouter } from "expo-router";
 import { Text, View } from "react-native";
 import ButtonComponent from "../base/Button";
 import { style } from "./style";
 import { cardProps } from "./useCardLogic";
 
-const OnboardingCard = ({ title, subtitle }: cardProps) => {
+const OnboardingCard = ({ title, subtitle, path }: cardProps) => {
+  const router = useRouter();
+  const handlePress = () => {
+    router.push(path);
+  };
+
   return (
     <View style={style.card}>
       <Text style={[style.text, typography.h1, typography.bold]}>{title}</Text>
       <Text style={style.text}>{subtitle}</Text>
       <View>
-        <ButtonComponent text="Next" mode="contained" />
+        <ButtonComponent text="Next" mode="contained" onPress={handlePress} />
       </View>
     </View>
   );
