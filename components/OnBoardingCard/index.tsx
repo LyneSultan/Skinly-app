a7c7f198492c6e06e804ee6471c326074b7aa712
LyneSultan <lynesultane@gmail.com>
2024-12-23 23:29:46 +0200

    updated onboarding component

--- components/OnBoardingCard/index.tsx
+++ components/OnBoardingCard/index.tsx
@@ -1,14 +1,18 @@
+import { typography } from "@/style/typography";
 import { Text, View } from "react-native";
 import ButtonComponent from "../base/Button";
+import { style } from "./style";
 
 const OnboardingCard = () => {
   return (
-    <View>
-      <Text>Know Your Ingredient</Text>
-      <Text>Learn what goes into your skincare </Text>
-      <ButtonComponent text="Next" mode="contained" />
-
+    <View style={style.card}>
+      <Text style={[style.text, typography.h1, typography.bold]}>Know Your Ingredient</Text>
+      <Text style={style.text}>Learn what goes into your skincare</Text>
+      <View>
+        <ButtonComponent text="Next" mode="contained" />
+      </View>
     </View>
-  )
-}
+  );
+};
+
 export default OnboardingCard;
