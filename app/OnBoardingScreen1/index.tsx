fdefddd0f61326838639439d3b147be9cdbd84bc
LyneSultan <lynesultane@gmail.com>
2024-12-23 22:18:41 +0200

    created first onBoarding page

---
+++ app/OnBoardingScreen1/index.tsx
@@ -0,0 +1,14 @@
+import { Image, View } from "react-native";
+
+const OnboardingScreen1 = () => {
+
+  return (
+    <View>
+      <View>
+        <Image source={require('@/assets/images/onboarding1.png')} />
+
+      </View>
+    </View>
+  )
+}
+export default OnboardingScreen1;
