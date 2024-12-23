48fe0ad6985687903b7ca93a81e4a48ff46227f6
LyneSultan <lynesultane@gmail.com>
2024-12-23 21:25:23 +0200

    added handleStart to welcome page

--- app/WelcomeScreen/index.tsx
+++ app/WelcomeScreen/index.tsx
@@ -2,15 +2,17 @@ import ButtonComponent from "@/components/base/Button";
 import { base } from "@/style/base";
 import { ImageBackground, Text, View } from "react-native";
 import { style } from "./style";
+import { useWelcomeLogic } from "./useWelcomeLogic";
 
 const WelcomeScreen = () => {
+  const { handleStart } = useWelcomeLogic();
   return (
     <ImageBackground
       source={require("./../../assets/images/welcome.png")}
       style={[style.background]}
       resizeMode="cover">
 
-      <View style={[base.flex, base.alignCenter, base.spaceAround, { height: '100%' }, base.default]}>
+      <View style={[base.flex, base.alignCenter, base.spaceAround, base.default]}>
 
         <View>
           <Text style={[style.title]}>Skinly</Text>
@@ -20,7 +22,7 @@ const WelcomeScreen = () => {
         <View></View>
 
         <View style={[style.buttonContainer]}>
-          <ButtonComponent text="Start" mode="contained" />
+          <ButtonComponent text="Start" mode="contained" onPress={handleStart} />
         </View>
 
       </View>
