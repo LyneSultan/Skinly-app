8c839c1fc99001ca1dd9d4af08d1b48f5fc788f6
LyneSultan <lynesultane@gmail.com>
2024-12-23 23:29:08 +0200

    created style file for onBoarding card

---
+++ components/OnBoardingCard/style.ts
@@ -0,0 +1,19 @@
+import { colors } from "@/colors/colors";
+import { base } from "@/style/base";
+import { StyleSheet } from "react-native";
+
+export const style= StyleSheet.create({
+  card: {
+    backgroundColor: colors.primary,
+    ...base.borderRadius,
+    gap: 20,
+    minHeight:200,
+    elevation: 4,
+    padding: 40,
+  },
+  text: {
+    fontSize: 14,
+    color: "#fff",
+    textAlign: "center",
+  },
+});
