a1ea77b7efaa97e2fc7276897393cc32a8c6e985
LyneSultan <lynesultane@gmail.com>
2024-12-24 16:57:44 +0200

    added images and updated colors

--- colors/colors.ts
+++ colors/colors.ts
@@ -1,5 +1,8 @@
 export const colors = {
-  primary: '#E5C1CD',
+  // primary: '#E5C1CD',
+  primary: '#D16F9A',
+  // secondary: '#D16F9A',
+
   secondary: '#CBDDD1',
   background: '#F0F0F0',
   rose:'#F5E6E8'
