4993fd653a673f1b2cb187941ddced57a18fe3d1
LyneSultan <lynesultane@gmail.com>
2024-12-24 17:27:40 +0200

    added product to product search page

--- app/(tabs)/ProductSearch/index.tsx
+++ app/(tabs)/ProductSearch/index.tsx
@@ -4,12 +4,37 @@ import { style } from "./style";
 
 const ProductSearch = () => {
   return (
-    <View style={[base.default]}>
+    <View style={[base.default, base.gap]}>
+
       <View style={[style.searchBar, base.flex, base.row, base.spaceBetween]}>
         <Text>Search for product by name</Text>
-
         <View><Image source={require('@/assets/images/search.png')} /></View>
       </View>
+
+      <View>
+        <Text>Product name</Text>
+      </View>
+
+      <View>
+        <View style={[base.flex, base.row]}>
+          <View>
+            <Image
+              source={{ uri: "https://cdn.shopify.com/s/files/1/2488/5102/products/Cicalphate_580x.jpg?v=1591268381 " }}
+              style={[style.productImage, base.borderRadius]}
+            />
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFp5ldaRoJHuCaQQraX1G-Y6_lXSLNtGBtpA&s" }}
+              style={[style.logo, base.borderRadius]}
+            />
+          </View>
+
+
+
+        </View>
+      </View>
+
     </View>
   )
 }
