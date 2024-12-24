ef5faa29c0ff31f659c9fcd601a407789553ecb3
LyneSultan <lynesultane@gmail.com>
2024-12-24 17:08:05 +0200

    added search bar to product page

--- app/(tabs)/ProductSearch/index.tsx
+++ app/(tabs)/ProductSearch/index.tsx
@@ -1,5 +1,15 @@
+import { base } from "@/style/base";
+import { Image, Text, View } from "react-native";
+
 const ProductSearch = () => {
+  return (
+    <View style={[base.default]}>
+      <View style={[base.flex, base.row, base.spaceBetween]}>
+        <Text>search for product by name</Text>
 
-
+        <View><Image source={require('@/assets/images/search.png')} /></View>
+      </View>
+    </View>
+  )
 }
 export default ProductSearch;
