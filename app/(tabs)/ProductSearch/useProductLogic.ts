b9b245ca0f9bb1deb3645a7f06a7f694e1e61900
LyneSultan <lynesultane@gmail.com>
2024-12-25 00:50:31 +0200

    added getProduct funstion

---
+++ app/(tabs)/ProductSearch/useProductLogic.ts
@@ -0,0 +1,23 @@
+export const useProductLogic = () => {
+  const getProduct = async () => {
+    try {
+      const response = await fetch(
+        "http://192.168.248.239:3000/product/common/Soskin Gentle Purifying Cleansing Gel"
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch product:", error);
+    }
+  };
+
+  return {
+    getProduct,
+  };
+};
