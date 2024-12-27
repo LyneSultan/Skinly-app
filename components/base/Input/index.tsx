4cf510bf248068a5352047ded87915f73069feb8
LyneSultan <lynesultane@gmail.com>
2024-12-27 15:25:59 +0200

    added helper text to input

--- components/base/Input/index.tsx
+++ components/base/Input/index.tsx
@@ -1,22 +1,33 @@
-import { TextInput } from 'react-native-paper';
+import { HelperText, TextInput } from 'react-native-paper';
 import { styles, theme } from './styles';
 
 type InputProps = {
   label: string;
   text: string;
   password?: boolean;
+  errorText?: string;
+  hasErrors?: boolean;
 }
 
-export const Input = ({ label, text, password }: InputProps) => {
+export const Input = ({ label, text, password, errorText, hasErrors }: InputProps) => {
   return (
-    <TextInput
-      label={label}
-      placeholder={text}
-      secureTextEntry={password}
-      mode="outlined"
-      outlineStyle={styles.outline}
-      theme={{ colors: theme }}
-      style={{ height: 45 }}
-    />
+    <>
+      <TextInput
+        label={label}
+        placeholder={text}
+        secureTextEntry={password}
+        mode="outlined"
+        outlineStyle={styles.outline}
+        theme={{ colors: theme }}
+        style={{ height: 45 }}
+        error={hasErrors}
+      />
+      {hasErrors && (
+        <HelperText type="error" visible={hasErrors} >
+          {errorText}
+        </HelperText>
+      )}
+
+    </>
   );
 };
