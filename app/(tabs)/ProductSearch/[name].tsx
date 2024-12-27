81d235f29cdb90f6348b3ecf3a99b118f9bad02c
LyneSultan <lynesultane@gmail.com>
2024-12-27 20:34:50 +0200

    added search bar to product page

--- app/(tabs)/ProductSearch/[name].tsx
+++ app/(tabs)/ProductSearch/[name].tsx
@@ -1,7 +1,9 @@
+import { colors } from '@/colors/colors';
 import { base } from '@/style/base';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 import { style } from './style';
 import { ProductDetails, useProductLogic } from './useProductLogic';
 
@@ -9,58 +11,70 @@ const ProductSearch = () => {
   const { name } = useLocalSearchParams();
   const { getProduct, productLink } = useProductLogic();
   const [productDetails, setProductDetails] = useState<ProductDetails[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
-        const details = await getProduct(name);
+        const details = await getProduct(searchQuery || name);
         setProductDetails(details);
       } catch (error) {
         console.error('Error fetching product details:', error);
       }
     };
     fetchProductDetails();
-  }, [name]);
+  }, [searchQuery, name]);
+
 
   return (
     <ScrollView style={[base.flex, base.column, base.default]}>
-      <View style={[base.flex, base.row, base.spaceAround]}>
+      <Searchbar
+        placeholder="Search"
+        value={searchQuery}
+        onChangeText={(text) => { setSearchQuery(text) }}
+        style={{ backgroundColor: colors.primary }} />
 
-        {productDetails.map((item) => (
-          <View style={[style.productContainer, base.flex, base.column, base.gap, base.justifyCenter]}>
-            <View>
-              <Image
-                source={{ uri: item.product.image }}
-                style={[style.productImage, base.borderRadius]}
-              />
-            </View>
-            <View style={[base.flex, base.row, base.spaceBetween]}>
-              <View>
-                <Image
-                  source={{ uri: item.company_logo }}
-                  style={[style.logo, base.borderRadius]}
-                />
+      {productDetails.length === 0 ? (
+        <View style={[base.flex, base.alignCenter, base.justifyCenter, { height: 200 }]}>
+          <Text style={{ color: colors.primary, fontSize: 16 }}>No products found</Text>
+        </View>)
+        : (
+          <View style={[base.flex, base.row, base.spaceAround, base.wrap, base.gap]}>
+            {productDetails.map((item, index) => (
+              <View style={[style.productContainer, base.flex, base.column, base.gap, base.justifyCenter]} >
+                <View>
+                  <Image
+                    source={{ uri: item.product.image }}
+                    style={[style.productImage, base.borderRadius]}
+                  />
+                </View>
+                <View style={[base.flex, base.row, base.spaceBetween]}>
+                  <View>
+                    <Image
+                      source={{ uri: item.company_logo }}
+                      style={[style.logo, base.borderRadius]}
+                    />
+                  </View>
+                  <View>
+                    <Text>{item.companyName}</Text>
+                  </View>
+                  <View>
+                    <Text>{item.product.price}</Text>
+                  </View>
+                </View>
+
+                <Text>{item.product.name}</Text>
+
+                <TouchableOpacity onPress={() => productLink(item.product.link)} style={base.alignCenter}>
+                  <Text>Learn more</Text>
+                </TouchableOpacity>
               </View>
-
-              <View>
-                <Text>{item.companyName}</Text>
-              </View>
-
-              <View>
-                <Text>{item.product.price}</Text>
-              </View>
-
-
-            </View>
-            <TouchableOpacity onPress={() => productLink(item.product.link)} style={base.alignCenter}>
-              <Text>Learn more</Text>
-            </TouchableOpacity>
-
+            ))}
           </View>
-        ))}
-      </View>
+        )}
     </ScrollView>
   );
-}
+
+};
 
 export default ProductSearch;
