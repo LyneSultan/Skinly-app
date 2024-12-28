2995c63c18db70908fff7e87525c192cb18c90f8
LyneSultan <lynesultane@gmail.com>
2024-12-28 16:13:27 +0200

    added text and search bar to company page

--- app/Company/index.tsx
+++ app/Company/index.tsx
@@ -1,8 +1,18 @@
-import { View } from "react-native";
+import { colors } from "@/colors/colors";
+import { base } from "@/style/base";
+import { typography } from "@/style/typography";
+import { Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 const Company = () => {
   return (
-    <View>
+    <View style={[base.default]}>
+      <View style={[base.gap]}>
+        <Text style={[typography.h1]}>Promote your products to users effectively</Text>
+        <Searchbar
+          placeholder="Search"
+          style={{ backgroundColor: colors.primary }} />
+      </View>
 
     </View>
   )
