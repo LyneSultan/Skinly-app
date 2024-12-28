c29178228713830e29bce86d188d2045fa456ee3
LyneSultan <lynesultane@gmail.com>
2024-12-28 22:12:44 +0200

    added handle register function

--- app/Register/index.tsx
+++ app/Register/index.tsx
@@ -1,14 +1,49 @@
 import ButtonComponent from "@/components/base/Button"
 import { Input } from "@/components/base/Input"
+import { routes } from "@/routes/server.routes"
 import { base } from "@/style/base"
 import { typography } from "@/style/typography"
-import { Link } from "expo-router"
+import axios from "axios"
+import { Link, router } from "expo-router"
+import { useState } from "react"
 import { Text, View } from "react-native"
 import { style } from "./style"
-import { useRegisterLogic } from "./useRegisterLogic"
 
 const Register = () => {
-  const { handleRegister } = useRegisterLogic();
+  // const { handleRegister } = useRegisterLogic();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  const handleRegister = async () => {
+    const payload = {
+      name,
+      password_confirmation,
+      email,
+      password,
+    };
+    console.log(apiUrl);
+
+    try {
+      const response = await axios.post(apiUrl + routes.register, payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // const token = response.data.access_token;
+
+      // await AsyncStorage.setItem('authToken', token);
+
+      // const storedToken = await AsyncStorage.getItem('authToken');
+      // console.log('Stored Token:', storedToken);
+      router.push('/(tabs)/HomeScreen')
+    } catch (error) {
+      console.error('Error Data:', error.response.data.message);
+    };
+  }
+
   return (
     <View style={[base.flex, base.alignCenter, base.default]}>
       <View style={[style.container]}>
@@ -18,10 +53,10 @@ const Register = () => {
         </View>
 
         <View style={style.registerForm}>
-          <Input label="name" text="enter your name" />
-          <Input label="email" text="enter your email" />
-          <Input label="password" text="enter your password" password={true} />
-          <Input label="Confirm password" text="enter your password again" />
+          <Input label="name" text="enter your name" onChangeText={(value) => setName(value)} />
+          <Input label="email" text="enter your email" onChangeText={(value) => setEmail(value)} />
+          <Input label="password" text="enter your password" password={true} onChangeText={(value) => setPassword(value)} />
+          <Input label="Confirm password" text="enter your password again" onChangeText={(value) => setPasswordConfirmation(value)} />
         </View>
         <ButtonComponent mode="contained" text="Register" onPress={handleRegister} />
 
