bcde8ae3f4b98c6cfd74fd55bf3b6c63237cfeed
LyneSultan <lynesultane@gmail.com>
2024-12-28 17:14:19 +0200

    added company's data to company page

--- app/Company/index.tsx
+++ app/Company/index.tsx
@@ -1,19 +1,52 @@
 import { colors } from "@/colors/colors";
 import { base } from "@/style/base";
 import { typography } from "@/style/typography";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { useCompanyLogic } from "./useCompanyLogic";
 
 const Company = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const { fetchData, setData, data } = useCompanyLogic();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedData = await fetchData();
+        setData(fetchedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getData();
+  }, []);
+
+
   return (
     <View style={[base.default]}>
       <View style={[base.gap]}>
         <Text style={[typography.h1]}>Promote your products to users effectively</Text>
         <Searchbar
-          placeholder="Search"
+          value={searchQuery}
+          onChangeText={(text) => { setSearchQuery(text) }}
+          placeholder="Enter the product name"
           style={{ backgroundColor: colors.primary }} />
-      </View>
 
+
+        <Text style={typography.h2}>Products</Text>
+
+        <View >
+          {data.length > 0 ? (
+            data.map((d) => (
+              <View style={[base.flex, base.row, base.wrap, base.alignCenter, base.spaceAround]} >
+                <Text>{d.name}</Text>
+              </View>
+            ))
+          ) : (<Text>Loading products...</Text>)}
+        </View>
+
+      </View>
     </View>
   )
 }
