64b32d397782b320fad4c53ff64cba4a6cd36b84
LyneSultan <lynesultane@gmail.com>
2024-12-28 17:32:08 +0200

    added company style file

---
+++ app/Company/style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+  productCard: {
+    width: "40%",
+    marginBottom:12,
+
+  },
+  text: {
+    width: "100%",
+    fontSize: 12
+  },
+  productImage: {
+    width: "100%",
+    height: 150,
+  }
+});
