5a563c8d960ffea0f15386262a9526b8d866c5e3
LyneSultan <lynesultane@gmail.com>
2024-12-28 23:19:32 +0200

    added used function in register page in useRegister logic file

--- app/Register/useRegisterLogic.ts
+++ app/Register/useRegisterLogic.ts
@@ -1,11 +1,50 @@
+import { routes } from "@/routes/server.routes";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 
 export const useRegisterLogic = () => {
   const router = useRouter();
-  const handleRegister = () => {
-    router.push('/HomeScreen')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  const handleRegister = async () => {
+    const payload = {
+      name,
+      password_confirmation,
+      email,
+      password,
+    };
+    console.log(apiUrl);
+
+    try {
+      const response = await axios.post(apiUrl + routes.register, payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const token = response.data.access_token;
+
+      await AsyncStorage.setItem('authToken', token);
+
+      const storedToken = await AsyncStorage.getItem('authToken');
+      console.log('Stored Token:', storedToken);
+
+      router.push('/(tabs)/HomeScreen')
+    } catch (error:any) {
+      console.error('Error Data:', error.response.data.message);
+    };
   }
+
   return {
+    setEmail,
+    setPassword,
+    setName,
+    setPasswordConfirmation,
     handleRegister,
   };
 }
