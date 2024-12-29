1c131d30198922eef9ba766e9962ddc5d11c8093
LyneSultan <lynesultane@gmail.com>
2024-12-30 01:32:54 +0200

    added ui to forget password page

--- app/ForgetPassword/index.tsx
+++ app/ForgetPassword/index.tsx
@@ -1,10 +1,36 @@
-import { View } from "react-native";
+import ButtonComponent from "@/components/base/Button";
+import { Input } from "@/components/base/Input";
+import { base } from "@/style/base";
+import { typography } from "@/style/typography";
+import { Image, Keyboard, Text, TouchableWithoutFeedback, View } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const ForgetPassword = () => {
   return (
-    <View>
+    <KeyboardAwareScrollView>
 
-    </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+        <View style={[base.default]} >
+          <View style={[base.flex, base.column, { gap: 60 }]}>
+
+            <View style={base.alignCenter}>
+              <Image source={require('@/assets/images/forgetPassword.png')} />
+            </View>
+
+            <View style={{ gap: 20 }}>
+              <Text style={typography.h1}>Forgot   Password?</Text>
+              <Text>Don’t worry ! It happens. Please enter the phone number we will send the OTP in this phone number.</Text>
+              <Input label="text" text="here" />
+
+              <ButtonComponent text="Vontine" />
+            </View>
+          </View>
+        </View >
+      </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
+
+
   )
 }
 export default ForgetPassword;
