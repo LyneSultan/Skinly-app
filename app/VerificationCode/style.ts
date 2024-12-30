217441ccd6ee7fede517e5883b7c6ca62499c045
LyneSultan <lynesultane@gmail.com>
2024-12-30 21:18:09 +0200

    added text styling for verifcation page

--- app/VerificationCode/style.ts
+++ app/VerificationCode/style.ts
@@ -2,6 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create(
   {
+    header: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
 
   }
 )
