c080f6eaf201111ec09e13f211dffa5c330f3676
LyneSultan <lynesultane@gmail.com>
2024-12-30 21:24:36 +0200

    updated verification page style

--- app/VerificationCode/index.tsx
+++ app/VerificationCode/index.tsx
@@ -1,19 +1,30 @@
+import ButtonComponent from '@/components/base/Button';
 import { base } from '@/style/base';
-import React from 'react';
 import { Image, Text, TextInput, View } from 'react-native';
+import { style } from './style';
 
 const VerificationCodePage = () => {
 
   return (
 
     <View style={[base.default]} >
-      <View style={[base.flex, base.column]}>
+      <View style={[base.flex, base.column, base.gap]}>
         <View style={base.alignCenter}>
           <Image source={require('@/assets/images/verificationCode.png')} style={{ width: "90%", height: 300 }} />
         </View>
-        <Text >Enter Verification Code</Text>
+        <Text style={style.header}>Enter Verification Code</Text>
+        <View style={[base.flex, base.row, base.justifyCenter, base.gap]}>
+          <TextInput keyboardType="numeric" style={style.inputBox} maxLength={1} />
+          <TextInput keyboardType="numeric" style={style.inputBox} maxLength={1} />
+          <TextInput keyboardType="numeric" style={style.inputBox} maxLength={1} />
+          <TextInput keyboardType="numeric" style={style.inputBox} maxLength={1} />
+
+        </View>
         <View>
-          <TextInput keyboardType="numeric" />
+          <Text style={[{ textAlign: "center" }]}>Don’t receive code ? Re-send</Text>
+        </View>
+        <View>
+          <ButtonComponent text='Submit' />
         </View>
       </View>
     </View>
