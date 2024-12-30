8f6f3ca57274115b102087e5f2d0193bb55f22e4
LyneSultan <lynesultane@gmail.com>
2024-12-30 15:11:19 +0200

    added components to advertisement page

--- app/AddAdvertisement/index.tsx
+++ app/AddAdvertisement/index.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/base/Input";
 import { base } from "@/style/base";
 import { typography } from "@/style/typography";
 import { useSearchParams } from "expo-router/build/hooks";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { style } from "./style";
 
 const AddvertisementPage = () => {
@@ -12,14 +12,33 @@ const AddvertisementPage = () => {
 
 
     <View style={[base.default]}>
-      <Text style={[typography.h1]}>Create Advertisement </Text>
-      <Text>Add Advertisement for: {productName}</Text>
-      <Input label="Advertisement Text" text="Write a catchy promotional message " />
-      <View style={[style.uploadImage]}></View>
-      <Text>Preview Advertisement</Text>
-      <View style={[style.previewCard]}></View>
-      <ButtonComponent text="Save" />
+      <View style={[base.gap]}>
+        <Text style={[typography.h1]}>Create Advertisement</Text>
+        <Text>Add Advertisement for: {productName}</Text>
+        <Input label="Advertisement Text" text="Write a catchy promotional message" />
+
+        <View style={[style.uploadImage, base.flex, base.row, base.alignCenter, base.spaceAround]}>
+          <Text style={{ flexShrink: 1, paddingRight: 10 }}>Upload an image to showcase your product</Text>
+          <Image
+            source={require('@/assets/images/upload.png')}
+            style={{ width: 50, height: 50, resizeMode: 'contain' }}
+          />
+        </View>
+
+        <Text style={{ marginTop: 20 }}>Preview Advertisement</Text>
+        <View style={[style.previewCard, base.flex, base.row, base.alignCenter, base.spaceAround]}>
+          <Image
+            source={require('@/assets/images/upload.png')}
+            style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          />
+          <Text style={{ flexShrink: 1, paddingRight: 10 }}>Upload an image to showcase your product</Text>
+
+        </View>
+
+        <ButtonComponent text="Save" />
+      </View>
     </View>
+
   )
 }
 export default AddvertisementPage;
