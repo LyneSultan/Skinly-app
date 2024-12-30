817fc462892d906954e336ee399b20fcf919afda
LyneSultan <lynesultane@gmail.com>
2024-12-30 21:18:40 +0200

    added input styling for verification page

--- app/VerificationCode/style.ts
+++ app/VerificationCode/style.ts
@@ -8,5 +8,21 @@ export const style = StyleSheet.create(
       marginBottom: 20,
     },
 
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '80%',
+    },
+    inputBox: {
+      width: 50,
+      height: 50,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      textAlign: 'center',
+      fontSize: 20,
+      borderRadius: 10,
+      marginHorizontal: 5,
+      backgroundColor: '#f5f5f5',
+    },
   }
 )
