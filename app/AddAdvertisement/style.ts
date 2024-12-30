e5f74a414c6702f43981e48c73c4e045f63dbdc5
LyneSultan <lynesultane@gmail.com>
2024-12-30 15:11:41 +0200

    added styling for ads page

--- app/AddAdvertisement/style.ts
+++ app/AddAdvertisement/style.ts
@@ -1,12 +1,23 @@
+import { colors } from "@/colors/colors";
+import { base } from "@/style/base";
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create(
   {
     previewCard: {
+      backgroundColor: colors.rose,
+      ...base.borderRadius,
+      padding: 15,
+      shadowColor: '#000',
+      elevation: 5,
 
     },
     uploadImage: {
-
-    }
+      ...base.borderRadius,
+      backgroundColor: colors.secondary ,
+      padding: 15,
+      shadowColor: '#000',
+      elevation: 5,
+    },
   }
 )
