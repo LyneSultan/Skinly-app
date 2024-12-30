11a5f67f763c61f2d5e2e71f0139d78e9b9aeba6
LyneSultan <lynesultane@gmail.com>
2024-12-30 14:34:56 +0200

    added component needed in advertisement page

--- app/AddAdvertisement/index.tsx
+++ app/AddAdvertisement/index.tsx
@@ -1,13 +1,24 @@
+import ButtonComponent from "@/components/base/Button";
+import { Input } from "@/components/base/Input";
+import { base } from "@/style/base";
+import { typography } from "@/style/typography";
 import { useSearchParams } from "expo-router/build/hooks";
 import { Text, View } from "react-native";
+import { style } from "./style";
 
 const AddvertisementPage = () => {
   const productName = useSearchParams(); // Retrieve the productName query parameter
   return (
 
 
-    <View>
+    <View style={[base.default]}>
+      <Text style={[typography.h1]}>Create Advertisement </Text>
       <Text>Add Advertisement for: {productName}</Text>
+      <Input label="Advertisement Text" text="Write a catchy promotional message " />
+      <View style={[style.uploadImage]}></View>
+      <Text>Preview Advertisement</Text>
+      <View style={[style.previewCard]}></View>
+      <ButtonComponent text="Save" />
     </View>
   )
 }
