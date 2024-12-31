085c11cd8fef77bf2669b6af37c4de83dc4b204f
LyneSultan <lynesultane@gmail.com>
2024-12-31 22:13:08 +0200

    deleted handle more function

--- app/(tabs)/HomeScreen/useHomeLogic.ts
+++ app/(tabs)/HomeScreen/useHomeLogic.ts
@@ -1,14 +1,15 @@
 import { fetch } from 'expo/fetch';
 import { useState } from 'react';
 
-const pageSize = 4;
-
+const pageSize = 12;
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 export const useHomeLogic = () => {
   const [data, setData] = useState<DataItem[]>([]);
   const [page, setPage] = useState(1);
- const fetchData = async () => {
+
+ const fetchData = async (page?:number) => {
    try {
-    const response = await fetch(`http://192.168.248.239:3000/product?page=1&pageSize=${pageSize}`);
+    const response = await fetch(`${apiUrl}/product?page=${page}&pageSize=${pageSize}`);
     const data = await response.json();
     return data;
 
@@ -17,20 +18,8 @@ export const useHomeLogic = () => {
   }
   };
 
-  const handleViewMore=async(page:number) => {
-    try {
-      const response = await fetch(`http://192.168.248.239:3000/product?page=${page}&pageSize=${pageSize}`);
-      const data = await response.json();
-      console.log(response);
-      return data;
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
   return {
     fetchData,
-    handleViewMore,
     data,
     page,
     setData,
