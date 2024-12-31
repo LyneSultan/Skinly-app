68f29f0acf161994c8e5c56c45f9955a06f47082
LyneSultan <lynesultane@gmail.com>
2024-12-31 22:22:11 +0200

    added advertisement in home page

--- app/(tabs)/HomeScreen/index.tsx
+++ app/(tabs)/HomeScreen/index.tsx
@@ -1,24 +1,33 @@
-import AdvertisementCard from "@/components/AddvertisementCard";
 import { base } from "@/style/base";
 import { typography } from "@/style/typography";
 import { Link, useRouter } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { style } from "./style";
 import { useHomeLogic } from "./useHomeLogic";
 
-
 const HomeScreen = () => {
-  const { fetchData, setData, data, handleViewMore, page, setPage } = useHomeLogic();
+  const { fetchData, setData, data, page, setPage } = useHomeLogic();
   const router = useRouter();
+  const [ads, setAds] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const fetchedData = await fetchData();
         setData(fetchedData);
+
+        const newAds = [];
+        fetchedData.forEach((d) =>
+          d.products?.forEach((product) => {
+            if (product.additional_info) {
+              newAds.push(product.additional_info.advertisement);
+            }
+          })
+        );
+        setAds(newAds);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.log(error.message);
       }
     };
     getData();
@@ -31,7 +40,17 @@ const HomeScreen = () => {
           <Text style={[typography.h1]}>Hello,</Text>
           <Text style={[typography.h2]}> Let’s take care of your skin!</Text>
 
-          <AdvertisementCard />
+          {ads.length > 0 && (
+            <View>
+              {ads.map((ad, index) => (
+                <Image
+                  key={index}
+                  source={{ uri: `http://192.168.10.128:3000/${ad}` }}
+                  style={{ width: 100, height: 100, marginVertical: 10 }}
+                />
+              ))}
+            </View>
+          )}
 
           <View style={[style.scanContainer, base.flex, base.row, base.spaceBetween, base.alignCenter]}>
             <Image source={require('@/assets/images/scan-logo.png')} />
@@ -77,7 +96,7 @@ const HomeScreen = () => {
             onPress={async () => {
               const current = page + 1;
               setPage(current);
-              const products = await handleViewMore(current);
+              const products = await fetchData(current);
               setData((prev) => [...prev, ...products]);
             }} >
             <Text >View More</Text>
@@ -90,4 +109,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
