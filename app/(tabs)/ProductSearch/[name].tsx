8ac63a0daed011bd6373ff5716e7d09e34059980
LyneSultan <lynesultane@gmail.com>
2025-01-01 23:06:24 +0200

    updated styling of search page

--- app/(tabs)/ProductSearch/[name].tsx
+++ app/(tabs)/ProductSearch/[name].tsx
@@ -27,51 +27,53 @@ const ProductSearch = () => {
 
 
   return (
-    <ScrollView style={[base.flex, base.column, base.default]}>
-      <Searchbar
-        placeholder="Search"
-        value={searchQuery}
-        onChangeText={(text) => { setSearchQuery(text) }}
-        style={{ backgroundColor: colors.primary }} />
+    <ScrollView style={[base.default]}>
+      <View style={[base.gap]}>
+        <Searchbar
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={(text) => { setSearchQuery(text) }}
+          style={{ backgroundColor: colors.primary }} />
 
-      {productDetails.length === 0 ? (
-        <View style={[base.flex, base.alignCenter, base.justifyCenter, { height: 200 }]}>
-          <Text style={{ color: colors.primary, fontSize: 16 }}>No products found</Text>
-        </View>)
-        : (
-          <View style={[base.flex, base.row, base.spaceAround, base.wrap, base.gap]}>
-            {productDetails.map((item, index) => (
-              <View style={[style.productContainer, base.flex, base.column, base.gap, base.justifyCenter]} >
-                <View>
-                  <Image
-                    source={{ uri: item.product.image }}
-                    style={[style.productImage, base.borderRadius]}
-                  />
-                </View>
-                <View style={[base.flex, base.row, base.spaceBetween]}>
+        {productDetails.length === 0 ? (
+          <View style={[base.flex, base.alignCenter, base.justifyCenter, { height: 200 }]}>
+            <Text style={{ color: colors.primary, fontSize: 16 }}>No products found</Text>
+          </View>)
+          : (
+            <View style={[base.flex, base.row, base.spaceAround, base.wrap]}>
+              {productDetails.map((item, index) => (
+                <View style={[style.productContainer, base.flex, base.column, base.gap]} key={index}>
                   <View>
                     <Image
-                      source={{ uri: item.company_logo }}
-                      style={[style.logo, base.borderRadius]}
+                      source={{ uri: item.product.image }}
+                      style={[style.productImage, base.borderRadius]}
                     />
                   </View>
-                  <View>
-                    <Text>{item.companyName}</Text>
+                  <View style={[base.flex, base.row, base.gap, { maxWidth: '60%' }]}>
+                    <View>
+                      <Image
+                        source={{ uri: item.company_logo }}
+                        style={[style.logo, base.borderRadius]}
+                      />
+                    </View>
+                    <View>
+                      <Text>{item.companyName}</Text>
+                    </View>
+                    <View>
+                      <Text>{item.product.price.split(' ')[0]}</Text>
+                    </View>
                   </View>
-                  <View>
-                    <Text>{item.product.price}</Text>
-                  </View>
+
+                  <Text>{item.product.name}</Text>
+
+                  <TouchableOpacity onPress={() => productLink(item.product.link)} style={base.alignCenter}>
+                    <Text>Learn more</Text>
+                  </TouchableOpacity>
                 </View>
-
-                <Text>{item.product.name}</Text>
-
-                <TouchableOpacity onPress={() => productLink(item.product.link)} style={base.alignCenter}>
-                  <Text>Learn more</Text>
-                </TouchableOpacity>
-              </View>
-            ))}
-          </View>
-        )}
+              ))}
+            </View>
+          )}
+      </View>
     </ScrollView>
   );
 
