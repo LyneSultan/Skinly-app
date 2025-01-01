62fb3c50715c471bf23198e93e0e63c473b4a764
LyneSultan <lynesultane@gmail.com>
2025-01-01 23:09:26 +0200

    added loading and response ui in ocr page

--- app/(tabs)/Ocr/index.tsx
+++ app/(tabs)/Ocr/index.tsx
@@ -1,26 +1,46 @@
 import ButtonComponent from '@/components/base/Button';
 import { base } from '@/style/base';
 import React from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { useOcrLogic } from './useOcrLogic';
 
 const Ocr = () => {
-  const { handleTakePicture, handlePickImage } = useOcrLogic();
+  const { apiResponse, handleTakePicture, handlePickImage, loading, setLoading } = useOcrLogic();
+
+  const handleWithLoading = async (action: any) => {
+    await action();
+  };
 
   return (
     <View style={[base.flex, base.default]}>
       <View style={[base.gap]}>
 
         <View>
-          <ButtonComponent text='Take picture' onPress={handleTakePicture} />
+          <ButtonComponent
+            text="Take picture"
+            onPress={() => handleWithLoading(handleTakePicture)}
+          />
         </View>
 
         <View>
-          <ButtonComponent text='Upload picture' mode='outlined' onPress={handlePickImage} />
+          <ButtonComponent
+            text="Upload picture"
+            mode="outlined"
+            onPress={() => handleWithLoading(handlePickImage)}
+          />
         </View>
 
+        {loading && (
+          <ActivityIndicator size="large" color='#D16F9A' style={{ marginTop: 20 }} />
+        )}
+
+        {apiResponse && (
+          <Text style={{ marginTop: 20 }}>
+            {typeof apiResponse === 'object' ? JSON.stringify(apiResponse, null, 2) : apiResponse}
+          </Text>
+        )}
       </View>
-    </View >
+    </View>
   );
 };
 
