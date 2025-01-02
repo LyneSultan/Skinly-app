a0f5148e39565d0c067cdb0a0ac73df1fa12a013
LyneSultan <lynesultane@gmail.com>
2025-01-02 20:29:39 +0200

    added functions in product logic file

--- app/(tabs)/ProductSearch/useProductLogic.ts
+++ app/(tabs)/ProductSearch/useProductLogic.ts
@@ -1,7 +1,30 @@
+import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Linking } from "react-native";
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 export const useProductLogic = () => {
+  const { name } = useLocalSearchParams();
+
+ const [productDetails, setProductDetails] = useState<ProductDetails[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  useEffect(() => {
+    setSearchQuery('');
+    console.log("enter or out")
+
+  }, [name]);
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        const details = await getProduct(searchQuery || name);
+        setProductDetails(details);
+      } catch (error) {
+        console.error('Error fetching product details:', error);
+      }
+    };
+    fetchProductDetails();
+  }, [searchQuery, name]);
+
   const getProduct = async (productName:string|string[]) => {
     try {
       const response = await fetch(`${apiUrl}/product/common/${productName}`);
@@ -23,7 +46,10 @@ export const useProductLogic = () => {
 
   return {
     getProduct,
-    productLink
+    productLink,
+    searchQuery,
+    setSearchQuery,
+    productDetails
   };
 };
 
