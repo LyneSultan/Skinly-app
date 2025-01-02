af4831e92c33d467959733b1c976099b01ea8fba
LyneSultan <lynesultane@gmail.com>
2025-01-02 20:29:04 +0200

    removed functions from product search file

--- app/(tabs)/ProductSearch/[name].tsx
+++ app/(tabs)/ProductSearch/[name].tsx
@@ -1,30 +1,11 @@
-import { colors } from '@/colors/colors';
 import { base } from '@/style/base';
-import { useLocalSearchParams } from 'expo-router';
-import { useEffect, useState } from 'react';
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { style } from './style';
-import { ProductDetails, useProductLogic } from './useProductLogic';
+import { useProductLogic } from './useProductLogic';
 
 const ProductSearch = () => {
-  const { name } = useLocalSearchParams();
-  const { getProduct, productLink } = useProductLogic();
-  const [productDetails, setProductDetails] = useState<ProductDetails[]>([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  useEffect(() => {
-    const fetchProductDetails = async () => {
-      try {
-        const details = await getProduct(searchQuery || name);
-        setProductDetails(details);
-      } catch (error) {
-        console.error('Error fetching product details:', error);
-      }
-    };
-    fetchProductDetails();
-  }, [searchQuery, name]);
-
+  const { productDetails, searchQuery, setSearchQuery, productLink } = useProductLogic();
 
   return (
     <ScrollView style={[base.default]}>
@@ -33,11 +14,11 @@ const ProductSearch = () => {
           placeholder="Search"
           value={searchQuery}
           onChangeText={(text) => { setSearchQuery(text) }}
-          style={{ backgroundColor: colors.primary }} />
+          style={style.searchBar} />
 
         {productDetails.length === 0 ? (
-          <View style={[base.flex, base.alignCenter, base.justifyCenter, { height: 200 }]}>
-            <Text style={{ color: colors.primary, fontSize: 16 }}>No products found</Text>
+          <View style={[base.flex, base.alignCenter, base.justifyCenter]}>
+            <Text style={style.notFoundText}>No products found</Text>
           </View>)
           : (
             <View style={[base.flex, base.row, base.spaceAround, base.wrap]}>
@@ -49,7 +30,7 @@ const ProductSearch = () => {
                       style={[style.productImage, base.borderRadius]}
                     />
                   </View>
-                  <View style={[base.flex, base.row, base.gap, { maxWidth: '60%' }]}>
+                  <View style={[base.flex, base.row, base.gap, { maxWidth: '50%', }]}>
                     <View>
                       <Image
                         source={{ uri: item.company_logo }}
