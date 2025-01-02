4a03b93559d2d69f04a2bb78d8774e78b86e10fd
LyneSultan <lynesultane@gmail.com>
2025-01-02 20:32:33 +0200

    added functions in home logic file

--- app/(tabs)/HomeScreen/useHomeLogic.ts
+++ app/(tabs)/HomeScreen/useHomeLogic.ts
@@ -1,36 +1,67 @@
 import { fetch } from 'expo/fetch';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const pageSize = 12;
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
 export const useHomeLogic = () => {
   const [data, setData] = useState<DataItem[]>([]);
   const [page, setPage] = useState(1);
+  const [ads, setAds] = useState<string[]>([]);
+  const newAds :string[] = [];
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedData = await fetchData();
+        setData((prev) => [...prev, ...fetchedData]);
+
+        fetchedData.forEach((d:DataItem) =>
+          d.products?.forEach((product) => {
+            if (product.additional_info) {
+              newAds.push(product.additional_info.advertisement);
+            }
+          })
+        );
+        setAds(() => [ ...newAds]);
+        console.log(ads);
+
+      } catch (error:any) {
+        console.log(error.message);
+      }
+    };
+    getData();
+  }, [page]);
 
  const fetchData = async (page?:number) => {
    try {
     const response = await fetch(`${apiUrl}/product?page=${page}&pageSize=${pageSize}`);
     const data = await response.json();
     return data;
-
   } catch (error) {
     console.error(error);
   }
   };
 
-  return {
-    fetchData,
-    data,
-    page,
-    setData,
-    setPage
+  const viewMore = async() => {
+    const current = page + 1;
+    setPage(current);
   }
 
+  return {
+    data,
+    page,
+    ads,
+    viewMore
+  }
 }
  type Product = {
   image: string;
   name: string;
-  price: string;
+   price: string;
+   additional_info?: {
+     advertisement:string,
+   }
 };
 
 export type DataItem = {
