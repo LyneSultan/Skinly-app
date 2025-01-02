25830acca9d0b67e18ff7d43a848ed47e773279f
LyneSultan <lynesultane@gmail.com>
2025-01-02 20:33:35 +0200

    created style file for password reset page

---
+++ app/PasswordReset/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  image: {
+    width: "90%", height: 300
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',  // Black color for subtitle
+    marginBottom: 20,  // Space between subtitle and other elements
+    textAlign: 'center', // Center the text for better alignment
+  },
+})
