1e9ab9bfde4902891098bf59a0e16011de16e509
LyneSultan <lynesultane@gmail.com>
2025-01-02 20:22:24 +0200

    updated requirement item

--- components/RequirementCard/index.tsx
+++ components/RequirementCard/index.tsx
@@ -1,6 +1,8 @@
 import { base } from '@/style/base';
+import { typography } from '@/style/typography';
 import React from 'react';
 import { Image, Text, View } from 'react-native';
+import { style } from './style';
 
 type RequirementItemProps = {
   image: any;
@@ -9,9 +11,9 @@ type RequirementItemProps = {
 
 const RequirementItem = ({ image, text }: RequirementItemProps) => {
   return (
-    <View style={[base.flex, base.row, base.alignCenter, base.gap]}>
-      <Image source={image} />
-      <Text style={base.whiteText}>{text}</Text>
+    <View style={[base.flex, base.row, base.alignCenter, style.card]}>
+      <Image source={image} style={style.icon} />
+      <Text style={[base.whiteText, typography.h2]}>{text}</Text>
     </View>
   );
 };
