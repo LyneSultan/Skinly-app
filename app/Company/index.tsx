c130ab0c62086874f27f9f99039bbe174e9ebebf
LyneSultan <lynesultane@gmail.com>
2025-01-06 11:36:33 +0200

    updated next and previous buttons

--- app/Company/index.tsx
+++ app/Company/index.tsx
@@ -3,8 +3,9 @@ import { base } from "@/style/base";
 import { typography } from "@/style/typography";
 import { Link } from "expo-router";
 import { useState } from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Searchbar } from "react-native-paper";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { style } from "./style";
 import { useCompanyLogic } from "./useCompanyLogic";
 
@@ -28,39 +29,60 @@ const Company = () => {
           <View>
             <View style={[base.flex, base.row, base.wrap, base.alignCenter, base.spaceAround]}>
               {data.length > 0 ? (
-                data.map((product, index) => (
-                  <Link href={`/AddAdvertisement?${product.name}`} style={[style.productsContainer]} key={index}>
+                data.map((product, index) => {
+                  const name = encodeURIComponent(product.name);
+                  console.log(name);
+                  return (
+                    <Link href={`/AddAdvertisement?productName=${name}`} style={[style.productsContainer]} key={index}>
 
-                    <View style={[style.productCard]} >
-                      <Image
-                        source={{ uri: product.image }}
-                        style={[style.productImage, base.borderRadius]}
-                      />
-                      <Text style={[style.text]} numberOfLines={2} ellipsizeMode="tail">
-                        {product.name}
-                      </Text>
-                    </View>
-                  </Link>
-                )
+                      <View style={[style.productCard]} >
+                        <Image
+                          source={{ uri: product.image }}
+                          style={[style.productImage, base.borderRadius]}
+                        />
+                        <Text style={[style.text]} numberOfLines={2} ellipsizeMode="tail">
+                          {product.name}
+                        </Text>
+                      </View>
+                    </Link>)
+                }
                 )) : (
                 <Text>Loading products...</Text>
               )}
             </View>
-            <View style={[base.flex, base.row, base.spaceAround]}>
-              <TouchableOpacity onPress={previousHandle}>
-                <Text>Previous</Text>
+
+            <View style={styles.paginationContainer}>
+              <TouchableOpacity onPress={previousHandle} style={styles.button}>
+                <Icon name="arrow-back-ios" size={24} color="black" />
+                <Text style={styles.text}>Previous</Text>
               </TouchableOpacity>
 
-              <TouchableOpacity onPress={nextHandle}>
-                <Text >Next</Text>
+              <TouchableOpacity onPress={nextHandle} style={styles.button}>
+                <Text style={styles.text}>Next</Text>
+                <Icon name="arrow-forward-ios" size={24} color="black" />
               </TouchableOpacity>
             </View>
-
           </View>
         </View>
       </View>
     </ScrollView>
   );
 };
-
+const styles = StyleSheet.create({
+  paginationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  text: {
+    marginHorizontal: 5,
+    fontSize: 16,
+    color: 'black',
+  },
+});
 export default Company;
