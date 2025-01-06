20457d265763c5a16ff48f963ccccf433ad2efd7
LyneSultan <lynesultane@gmail.com>
2025-01-06 12:07:38 +0200

    added requirement card to ocr page

--- app/(tabs)/Ocr/index.tsx
+++ app/(tabs)/Ocr/index.tsx
@@ -1,43 +1,48 @@
 import ButtonComponent from '@/components/base/Button';
+import { StepItem } from '@/components/StepItem';
 import { base } from '@/style/base';
+import { typography } from '@/style/typography';
 import React from 'react';
 import { ActivityIndicator, Text, View } from 'react-native';
+import { style } from './style';
 import { useOcrLogic } from './useOcrLogic';
 
 const Ocr = () => {
-  const { apiResponse, handleTakePicture, handlePickImage, loading } = useOcrLogic();
-
+  const { loading, handlePickImage, handleTakePicture, apiResponse } = useOcrLogic();
   return (
     <View style={[base.flex, base.default]}>
-      <View style={[base.gap]}>
-
-        <View>
-          <ButtonComponent
-            text="Take picture"
-            onPress={() => handleTakePicture}
-          />
-        </View>
-
-        <View>
-          <ButtonComponent
-            text="Upload picture"
-            mode="outlined"
-            onPress={() => handlePickImage}
-          />
-        </View>
-
-        {loading && (
-          <ActivityIndicator size="large" color='#D16F9A' style={{ marginTop: 20 }} />
-        )}
-
-        {apiResponse && (
-          <Text style={{ marginTop: 20 }}>
-            {typeof apiResponse === 'object' ? JSON.stringify(apiResponse, null, 2) : apiResponse}
-          </Text>
-        )}
+      <View>
+        <Text style={style.title}>
+          Discover what ingredients suit your skin best. Follow the steps below to get started.
+        </Text>
       </View>
+
+      <View style={[style.requirementCard, base.flex, base.row, base.gap]}>
+
+        <View style={style.container}>
+          <Text style={[typography.h1, base.whiteText, typography.bold]}> Snap, Scan, Transform!</Text>
+          <StepItem stepNumber={1} text="Ensure Ingredients are Readable" image={require('@/assets/images/face1.png')} />
+          <StepItem stepNumber={2} text="Capture or Upload the Image" image={require('@/assets/images/foundation.png')} />
+          <StepItem stepNumber={3} text="Wait for Results" image={require('@/assets/images/sun.png')} />
+        </View>
+
+      </View>
+      <View style={base.gap}>
+        <ButtonComponent text="Take Picture" onPress={handleTakePicture} disabled={loading} />
+        <ButtonComponent text="Upload Picture" mode="outlined" onPress={handlePickImage} disabled={loading} />
+      </View>
+
+      {loading && (
+        <ActivityIndicator size="large" color="#D16F9A" style={{ marginTop: 20 }} />
+      )}
+
+      {apiResponse && (
+        <Text>{apiResponse.data}</Text>
+      )}
     </View>
   );
 };
 
 export default Ocr;
+
+
