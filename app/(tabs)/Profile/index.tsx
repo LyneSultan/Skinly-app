21a66d4c99f8b431ca34e3ebbf9649efcca91a78
LyneSultan <lynesultane@gmail.com>
2025-01-06 11:51:19 +0200

    added labels to profile page

--- app/(tabs)/Profile/index.tsx
+++ app/(tabs)/Profile/index.tsx
@@ -1,10 +1,40 @@
+import { base } from "@/style/base";
+import React from "react";
 import { Text, View } from "react-native";
+import { style } from "./style";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={[base.default, base.flex]}>
+      <View style={[style.container, base.alignCenter, base.justifyCenter]}>
+        <View style={style.card}>
+          <View style={[style.details, base.gap]}>
+            <View>
+              <Text style={style.label}>first name</Text>
+              <Text style={style.value}>Lyne</Text>
+            </View>
+
+            <View>
+              <Text style={style.label}>Last name</Text>
+              <Text style={style.value}>Al-Sultan</Text>
+            </View>
+
+            <View>
+              <Text style={style.label}>Email</Text>
+              <Text style={style.value}>Lynesultane@gmail.com</Text>
+            </View>
+            <View>
+              <Text style={style.label}>Skin type</Text>
+              <Text style={style.value}>normal</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+
+};
+
+
+
 export default Profile;
