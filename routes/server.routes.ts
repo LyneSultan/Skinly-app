43368735c6e2174900d4e8405e689d29519560d5
LyneSultan <lynesultane@gmail.com>
2025-01-08 21:12:29 +0200

    added routes to routes file

--- routes/server.routes.ts
+++ routes/server.routes.ts
@@ -1,7 +1,10 @@
 export const routes = {
   login: "/auth/login",
   register: "/auth/register",
+  veificationCode: "/auth/sendCode",
   skinDetection: "/skinDetection",
-  ocr:"/ocr"
-
-}
+  ocr: "/ocr",
+  advertisement: (productName: string) => "/advertisement/" + productName,
+  getCompanyProducts: "/product/company",
+  getProducts: (page: number, pageSize: number) => `/product?page=${page}&pageSize=${pageSize}`,
+ }
