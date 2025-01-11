3ba47271f03abd92761709ff6c22e195984f5361
LyneSultan <lynesultane@gmail.com>
2025-01-11 10:36:42 +0200

    added logo and title to login page

--- app/Login/index.tsx
+++ app/Login/index.tsx
@@ -1,3 +1,4 @@
+import { colors } from "@/colors/colors"
 import ButtonComponent from "@/components/base/Button"
 import { Input } from "@/components/base/Input"
 import { base } from "@/style/base"
@@ -11,46 +12,49 @@ const Login = () => {
   const { setEmail, setPassword, handleLogin, errorMessages, loading } = useLoginLogic();
 
   return (
-    <View style={[base.flex, base.alignCenter, base.default]}>
-      <View style={[style.container]}>
-        <View style={[base.alignCenter,]}>
-          <Image source={require('@/assets/images/Project logo.png')} style={[style.loginImage]} />
-        </View>
-
-        <View>
-          <Text style={typography.h1}>Welcome back! Glad to see you, Again!</Text>
-        </View>
-
-        <View style={style.loginForm}>
-          <Input label="Email" text="Enter your email" onChangeText={(value) => setEmail(value)} />
-          <Input label="Password" text="Enter your password" password={true} onChangeText={(password) => setPassword(password)} />
-        </View>
-
-        <View style={base.flex}>
-          <View style={style.forgetPassword}>
-            <Link href="/ForgetPassword">
-              <Text>Forgot Password?</Text>
-            </Link>
+    <View style={{ backgroundColor: colors.background }}>
+      <View style={[base.flex, base.alignCenter, base.default]}>
+        <View style={[style.container]}>
+          <View style={[base.alignCenter, base.flex, base.row, base.gap]}>
+            <Image source={require('@/assets/images/logo.png')} style={[style.loginImage]} />
+            <Text style={[style.title]}>Skinly</Text>
           </View>
-        </View>
 
-        {errorMessages.length > 0 && (
-          <View >
-            {errorMessages.map((msg, index) => (
-              <Text key={index} style={style.errorText}>
-                * {msg}
-              </Text>
-            ))}
+          <View>
+            <Text style={typography.h1}>Welcome back! Glad to see you, Again!</Text>
           </View>
-        )}
 
-        <ButtonComponent mode="contained" text="Login" onPress={handleLogin} disabled={loading} />
+          <View style={style.loginForm}>
+            <Input label="Email" text="Enter your email" onChangeText={(value) => setEmail(value)} />
+            <Input label="Password" text="Enter your password" password={true} onChangeText={(password) => setPassword(password)} />
+          </View>
+
+          <View style={base.flex}>
+            <View style={style.forgetPassword}>
+              <Link href="/ForgetPassword">
+                <Text>Forgot Password?</Text>
+              </Link>
+            </View>
+          </View>
+
+          {errorMessages.length > 0 && (
+            <View >
+              {errorMessages.map((msg, index) => (
+                <Text key={index} style={style.errorText}>
+                  * {msg}
+                </Text>
+              ))}
+            </View>
+          )}
+
+          <ButtonComponent mode="contained" text="Login" onPress={handleLogin} disabled={loading} />
 
 
-        <View style={[base.flex, base.row, base.justifyCenter]}>
-          <Text>Don ‘t have an account? <Link href="/Register"><Text style={[style.login, typography.bold]}>Register</Text> </Link> </Text>
-        </View>
-      </View >
+          <View style={[base.flex, base.row, base.justifyCenter]}>
+            <Text>Don ‘t have an account? <Link href="/Register"><Text style={[style.login, typography.bold]}>Register</Text> </Link> </Text>
+          </View>
+        </View >
+      </View>
     </View>
   )
 }
