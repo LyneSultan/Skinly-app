cb27ebab110d5ef4f77bf70433927d6ed34baa17
LyneSultan <lynesultane@gmail.com>
2025-01-11 10:54:58 +0200

    added error handling to verfication function

--- app/VerificationCode/useVerificationLogic.ts
+++ app/VerificationCode/useVerificationLogic.ts
@@ -1,12 +1,29 @@
 import { routes } from "@/routes/server.routes";
 import axios from "axios";
+import { router } from "expo-router";
+import { useState } from "react";
 
 const useVerifcationLogic = () => {
+  const [error, setError] = useState("");
+
   const senCode = async (email: string) => {
+    setError("");
+    if (!email) {
+      setError("Email is required.");
+      return;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
     try {
       const response = await axios.post(process.env.EXPO_PUBLIC_API_URL + routes.veificationCode, {
         email,
       })
+      const code = response.data;
+      router.push(`/VerificationCode?code=${code}`)
+
       console.log(response.data);
       return response.data;
     } catch (error) {
@@ -15,6 +32,7 @@ const useVerifcationLogic = () => {
   }
   return {
     senCode,
+    error
   }
 }
 export default  useVerifcationLogic;
