7f58fb90efc266fde1ff8f02db3f27a2202a8951
LyneSultan <lynesultane@gmail.com>
2025-01-11 10:37:05 +0200

    updated register styling

--- app/Register/style.ts
+++ app/Register/style.ts
@@ -1,13 +1,22 @@
 import { colors } from "@/colors/colors";
+import { typography } from "@/style/typography";
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
+  title: {
+        fontSize:42,
+        ...typography.bold,
+        color:colors.primary
+      },
+    loginImage: {
+      width: 60, height: 60
+    },
   container: {
     width: "100%",
-    gap: 20,
+    gap: 10,
   },
   registerForm: {
-    gap:7
+    gap:4
   },
   register: {
     color:colors.primary
