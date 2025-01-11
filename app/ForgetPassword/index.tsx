264b833d06cd0fd37c1601df5e8f00373147cfa9
LyneSultan <lynesultane@gmail.com>
2025-01-11 10:56:03 +0200

    added error ui in forget password

--- app/ForgetPassword/index.tsx
+++ app/ForgetPassword/index.tsx
@@ -1,3 +1,4 @@
+import { colors } from "@/colors/colors";
 import ButtonComponent from "@/components/base/Button";
 import { Input } from "@/components/base/Input";
 import { base } from "@/style/base";
@@ -10,32 +11,33 @@ import useVerifcationLogic from "../VerificationCode/useVerificationLogic";
 const ForgetPassword = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
-  const { senCode } = useVerifcationLogic();
+  const { senCode, error } = useVerifcationLogic();
 
   return (
-    <KeyboardAvoidingView
-    >
+    <KeyboardAvoidingView>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+        <View style={{ backgroundColor: colors.background }}>
 
-        <View style={[base.default,]} >
-          <View style={[base.flex, base.column, base.gap]}>
+          <View style={[base.default,]} >
+            <View style={[base.flex, base.column, base.gap]}>
 
-            <View style={base.alignCenter}>
-              <Image source={require('@/assets/images/forgetPassword.png')} style={{ width: "90%", height: 300 }} />
+              <View style={base.alignCenter}>
+                <Image source={require('@/assets/images/forgetPassword.png')} style={{ width: "90%", height: 300 }} />
+              </View>
+
+              <View style={[base.gap]}>
+                <Text style={typography.h1}>Forgot Password?</Text>
+                <Text>Don’t worry ! It happens. Please enter your email we will send you a verification code.</Text>
+                <Input label="Email" text="Enter Your Email" onChangeText={(text) => setEmail(text)} />
+                {error && (
+                  <Text style={{ color: colors.error }}>{error}</Text>
+                )}
+                <ButtonComponent text="Contine" onPress={() => senCode(email)} />
+              </View>
+
             </View>
-
-            <View style={[base.gap]}>
-              <Text style={typography.h1}>Forgot Password?</Text>
-              <Text>Don’t worry ! It happens. Please enter your email we will send you a verification code.</Text>
-              <Input label="email" text="Enter your email" onChangeText={(text) => setEmail(text)} />
-              <ButtonComponent text="Contine" onPress={async () => {
-                const code = await senCode(email);
-                router.push(`/VerificationCode?code=${code}`)
-              }} />
-            </View>
-
-          </View>
-        </View >
+          </View >
+        </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   )
