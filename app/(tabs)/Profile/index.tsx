2c800b49147f855f17eb514d822fee7aec14461e
LyneSultan <lynesultane@gmail.com>
2025-01-12 16:09:27 +0200

    added profile field to profile page

--- app/(tabs)/Profile/index.tsx
+++ app/(tabs)/Profile/index.tsx
@@ -1,7 +1,8 @@
 import { colors } from "@/colors/colors";
+import ProfileField from "@/components/ProfileField";
 import { base } from "@/style/base";
 import React, { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { style } from "./style";
 
 const Profile = () => {
@@ -25,63 +26,40 @@ const Profile = () => {
       <View style={[base.default, base.flex]}>
         <View style={[style.container, base.alignCenter, base.justifyCenter]}>
           <View style={style.card}>
+
             <View style={[base.flex, base.row, base.spaceBetween]}>
               <Text style={style.title}>Personal Details</Text>
               <TouchableOpacity onPress={handleEditToggle}>
                 <Text style={style.editButton}>{isEditing ? "save" : "edit"}</Text>
               </TouchableOpacity>
             </View>
+
             <View style={[style.details, base.gap]}>
-              <View>
-                <Text style={style.label}>first name</Text>
-                {isEditing ? (
-                  <TextInput
-                    style={style.input}
-                    value={formData.firstName}
-                    onChangeText={(text) => handleChange("firstName", text)}
-                  />
-                ) : (
-                  <Text style={style.value}>{formData.firstName}</Text>
-                )}
-              </View>
 
-              <View>
-                <Text style={style.label}>Last name</Text>
-                {isEditing ? (
-                  <TextInput
-                    style={style.input}
-                    value={formData.lastName}
-                    onChangeText={(text) => handleChange("lastName", text)}
-                  />
-                ) : (
-                  <Text style={style.value}>{formData.lastName}</Text>
-                )}
-              </View>
-
-              <View>
-                <Text style={style.label}>Email</Text>
-                {isEditing ? (
-                  <TextInput
-                    style={style.input}
-                    value={formData.email}
-                    onChangeText={(text) => handleChange("email", text)}
-                  />
-                ) : (
-                  <Text style={style.value}>{formData.email}</Text>
-                )}
-              </View>
-              <View>
-                <Text style={style.label}>Skin type</Text>
-                {isEditing ? (
-                  <TextInput
-                    style={style.input}
-                    value={formData.skinType}
-                    onChangeText={(text) => handleChange("skinType", text)}
-                  />
-                ) : (
-                  <Text style={style.value}>{formData.skinType}</Text>
-                )}
-              </View>
+             <ProfileField
+                label="First name"
+                value={formData.firstName}
+                isEditing={isEditing}
+                onChange={(text) => handleChange("firstName", text)}
+              />
+              <ProfileField
+                label="Last name"
+                value={formData.lastName}
+                isEditing={isEditing}
+                onChange={(text) => handleChange("lastName", text)}
+              />
+              <ProfileField
+                label="Email"
+                value={formData.email}
+                isEditing={isEditing}
+                onChange={(text) => handleChange("email", text)}
+              />
+              <ProfileField
+                label="Skin type"
+                value={formData.skinType}
+                isEditing={isEditing}
+                onChange={(text) => handleChange("skinType", text)}
+              />
             </View>
           </View>
         </View>
