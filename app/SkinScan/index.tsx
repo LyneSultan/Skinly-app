dfecf074f20bdf60f2133ac697ec15c55865c402
LyneSultan <lynesultane@gmail.com>
2025-01-12 15:42:16 +0200

    added scrolla nd suggestions

--- app/SkinScan/index.tsx
+++ app/SkinScan/index.tsx
@@ -4,58 +4,63 @@ import RequirementItem from '@/components/RequirementCard';
 import { base } from '@/style/base';
 import { typography } from '@/style/typography';
 import React from 'react';
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { ActivityIndicator } from 'react-native-paper';
 import { style } from './style';
 import { useSkinPageLogic } from './useSkinPageLogic';
 
 const SkinScan = () => {
-  const { handlePickImage, handleTakePicture, imageMap, apiResponse, loading } = useSkinPageLogic();
+  const { handlePickImage, suggestions, handleTakePicture, imageMap, apiResponse, loading } = useSkinPageLogic();
 
   return (
-    <View style={[base.flex, base.default]}>
-      <View style={[base.gap]}>
-        <Text style={[typography.h1]}>One click away from personalized skincare insights</Text>
-        <View style={[style.requirementCard]}>
-          <View style={[base.flex, base.alignCenter,]}>
-            <Text style={[typography.h1, base.whiteText, typography.bold]}> Snap, Scan, Transform!</Text>
+    <ScrollView>
+      <View style={[base.flex, base.default]}>
+        <View style={[base.gap]}>
+          <Text style={[typography.h1]}>One click away from personalized skincare insights</Text>
+          <View style={[style.requirementCard]}>
+            <View style={[base.flex, base.alignCenter,]}>
+              <Text style={[typography.h1, base.whiteText, typography.bold]}> Snap, Scan, Transform!</Text>
+            </View>
+
+            <View style={[base.flex, base.column, base.gap]}>
+              <RequirementItem text='Relax your face' image={require('@/assets/images/face1.png')} />
+              <RequirementItem text='Do not apply any products' image={require('@/assets/images/foundation.png')} />
+              <RequirementItem text='Sit in a good lighting' image={require('@/assets/images/sun.png')} />
+            </View>
           </View>
 
-          <View style={[base.flex, base.column, base.gap]}>
-            <RequirementItem text='Relax your face' image={require('@/assets/images/face1.png')} />
-            <RequirementItem text='Do not apply any products' image={require('@/assets/images/foundation.png')} />
-            <RequirementItem text='Sit in a good lighting' image={require('@/assets/images/sun.png')} />
-          </View>
-        </View>
+          <ButtonComponent text='Take picture' onPress={handleTakePicture} disabled={loading} />
 
-        <ButtonComponent text='Take picture' onPress={handleTakePicture} disabled={loading} />
+          <ButtonComponent text='Upload picture' mode='outlined' onPress={handlePickImage} disabled={loading} />
+          {loading && (
+            <ActivityIndicator size="large" color='#D16F9A' style={{ marginTop: 20 }} />
+          )}
 
-        <ButtonComponent text='Upload picture' mode='outlined' onPress={handlePickImage} disabled={loading} />
-        {loading && (
-          <ActivityIndicator size="large" color='#D16F9A' style={{ marginTop: 20 }} />
-        )}
-
-        {apiResponse && (
-          <>
-            {apiResponse.error && (
-              <Text style={style.errorText}>An error occurred. Please retry again.</Text>
-            )}
-            {apiResponse.class && (
-              <View style={[base.flex, base.row, base.alignCenter, { padding: 15 }]} >
-                <Text style={[typography.h1, typography.bold, { flex: 1 }]}>
-                  Your skin type is
-                  <Text style={{ color: colors.primary }}>
-                    {apiResponse.class.charAt(0).toUpperCase() + apiResponse.class.slice(1)}
+          {apiResponse && (
+            <>
+              {apiResponse.error && (
+                <Text style={style.errorText}>An error occurred. Please retry again.</Text>
+              )}
+              {apiResponse.class && (
+                <View style={[base.flex, base.row, base.alignCenter, { padding: 15 }]} >
+                  <Text style={[typography.h1, typography.bold, { flex: 1 }]}>
+                    Your skin type is
+                    <Text style={{ color: colors.primary }}>
+                      {" " + apiResponse.class}
+                    </Text>
                   </Text>
-                </Text>
-                <Image source={imageMap[apiResponse.class]} style={style.skinTypeImage} />
-              </View>
-            )}
-          </>
-        )}
+                  <Image source={imageMap[apiResponse.class as keyof typeof imageMap]} style={style.skinTypeImage} />
+                </View>
+              )}
+            </>
+          )}
+          {suggestions && (
+            < Text > {suggestions}</Text>
+          )}
 
-      </View>
-    </View >
+        </View>
+      </View >
+    </ScrollView >
   );
 }
 export default SkinScan;
