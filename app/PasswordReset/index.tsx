3d69b72ba0d48fa1e415a151c9149bb4caeaa7cd
LyneSultan <lynesultane@gmail.com>
2025-01-12 15:43:03 +0200

    updated reset page

--- app/PasswordReset/index.tsx
+++ app/PasswordReset/index.tsx
@@ -1,26 +1,36 @@
+import { colors } from "@/colors/colors";
 import ButtonComponent from "@/components/base/Button";
 import { Input } from "@/components/base/Input";
 import { base } from "@/style/base";
 import { Image, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { style } from "./style";
+import useResetLogic from "./useResetLogic";
 
 const PasswordResetPage = () => {
+  const { handleReset, setPassword, setConfirmPassword } = useResetLogic();
   return (
-    <View style={[base.default]} >
-      <View style={[base.flex, base.column]}>
+    <KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
 
-        <View style={base.alignCenter}>
-          <Image source={require('@/assets/images/passwordReset.png')} style={style.image} />
-        </View>
-        <Text style={style.subtitle}>Reset Your Password</Text>
+      <View style={{ backgroundColor: colors.background }}>
 
-        <View style={[base.gap]}>
-          <Input text="Password" label="password" />
-          <Input text="confirm password" label="password confirmation" />
-          <ButtonComponent text="Save" />
+        <View style={[base.default]} >
+          <View style={[base.flex, base.column]}>
+
+            <View style={base.alignCenter}>
+              <Image source={require('@/assets/images/passwordReset.png')} style={style.image} />
+            </View>
+            <Text style={style.subtitle}>Reset Your Password</Text>
+
+            <View style={[base.gap]}>
+              <Input text="Password" label="password" onChangeText={(text) => setPassword(text)} />
+              <Input text="confirm password" label="password confirmation" onChangeText={(text) => setConfirmPassword(text)} />
+              <ButtonComponent text="Save" onPress={handleReset} />
+            </View>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
 
   )
 }
