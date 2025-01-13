4b80568150f8a39c99a414c328be58c285bca38c
LyneSultan <lynesultane@gmail.com>
2025-01-13 15:54:43 +0200

    updated login styling

--- app/Login/style.ts
+++ app/Login/style.ts
@@ -1,35 +1,41 @@
-
 import { colors } from '@/colors/colors';
 import { typography } from '@/style/typography';
 import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
-
   container: {
     gap: 20,
-    width:"100%"
+    width: '100%',
   },
-   title: {
-      marginVertical: 20,
-      fontSize:42,
-      ...typography.bold,
-      color:colors.primary
-    },
+  title: {
+    marginVertical: 20,
+    fontSize: 42,
+    ...typography.bold,
+    color: colors.primary,
+  },
   loginImage: {
-    width: 60, height: 60
+    width: 60,
+    height: 60,
   },
   loginForm: {
-    gap:10
+    gap: 10,
   },
   forgetPassword: {
-    alignSelf:"flex-end"
+    alignSelf: 'flex-end',
   },
   login: {
     color: colors.primary,
   },
   errorText: {
-    color:colors.error
+    color: colors.error,
   },
-
-
-})
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: 8,
+  },
+  showPasswordText: {
+    marginLeft: 8,
+  },
+});
