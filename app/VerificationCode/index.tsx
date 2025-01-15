9d85527d10e83eef1d967f6f09dc90dfcf42a295
LyneSultan <lynesultane@gmail.com>
2025-01-16 01:59:03 +0200

    added handle input change function

--- app/VerificationCode/index.tsx
+++ app/VerificationCode/index.tsx
@@ -1,21 +1,34 @@
 import { colors } from '@/colors/colors';
 import ButtonComponent from '@/components/base/Button';
 import { base } from '@/style/base';
+import React, { useRef } from 'react';
 import { Image, Text, TextInput, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import useForgetLogic from '../ForgetPassword/useForgetLogic';
 import { style } from './style';
 import useVerificationLogic from './useVerificationLogic';
 
 const VerificationCodePage = () => {
-  const { inputs, handleInputChange, handleSubmit } = useVerificationLogic();
+  const { inputs, handleInputChange, handleSubmit, email } = useVerificationLogic();
+  const inputRefs = useRef([]);
+  const { senCode } = useForgetLogic();
+  const handleInputChangeWithFocus = (text, index) => {
+    handleInputChange(text, index);
+    if (text && index < inputs.length - 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
   return (
     <KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
       <View style={{ backgroundColor: colors.background }}>
-
         <View style={[base.default]}>
           <View style={[base.flex, base.column, base.gap]}>
             <View style={base.alignCenter}>
-              <Image source={require('@/assets/images/verificationCode.png')} style={style.image} />
+              <Image
+                source={require('@/assets/images/verificationCode.png')}
+                style={style.image}
+              />
             </View>
             <Text style={style.header}>Enter Verification Code</Text>
 
@@ -27,12 +40,16 @@ const VerificationCodePage = () => {
                   style={style.inputBox}
                   maxLength={1}
                   value={inputValue}
-                  onChangeText={(text) => handleInputChange(text, index)}
+                  onChangeText={(text) => handleInputChangeWithFocus(text, index)}
+                  ref={(el) => (inputRefs.current[index] = el)}
                 />
               ))}
             </View>
+            {/* <TouchableOpacity onPress={async() => senCode(email)}>
+              <Text style={style.resendText}>Don’t receive code? Re-send</Text>
 
-            <Text style={style.resendText}>Don’t receive code? Re-send</Text>
+            </TouchableOpacity> */}
+
 
             <ButtonComponent text="Submit" onPress={handleSubmit} />
           </View>
