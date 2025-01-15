220dd7e38af39e38d499fd564b033ee01b39ee9b
LyneSultan <lynesultane@gmail.com>
2025-01-16 01:59:47 +0200

    updated register styling

--- app/Register/style.ts
+++ app/Register/style.ts
@@ -1,30 +1,37 @@
-import { colors } from "@/colors/colors";
-import { typography } from "@/style/typography";
-import { StyleSheet } from "react-native";
+import { colors } from '@/colors/colors';
+import { typography } from '@/style/typography';
+import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
   title: {
-        fontSize:42,
-        ...typography.bold,
-        color:colors.primary
-      },
-    loginImage: {
-      width: 60, height: 60
-    },
+    fontSize: 42,
+    ...typography.bold,
+    color: colors.primary,
+  },
+  loginImage: {
+    width: 60,
+    height: 60,
+  },
   container: {
-    width: "100%",
+    width: '100%',
     gap: 10,
   },
   registerForm: {
-    gap:4
+    gap: 10,
   },
   register: {
-    color:colors.primary
+    color: colors.primary,
   },
   error: {
-    color:colors.error
-  }
-
-})
-
-
+    color: colors.error,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    marginLeft: 8,
+  },
+  showPasswordText: {
+    marginLeft: 8,
+  },
+});
