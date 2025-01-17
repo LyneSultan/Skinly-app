2069322d88581e55aaab165b7d4e2de35e1807ba
LyneSultan <lynesultane@gmail.com>
2025-01-17 13:03:42 +0200

    updated product card

--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -1,3 +1,4 @@
+import { colors } from "@/colors/colors";
 import { base } from "@/style/base";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import Icon from 'react-native-vector-icons/Feather';
@@ -17,32 +18,48 @@ type ProductCardProps = {
   onLinkPress: (link: string) => void;
 }
 const ProductCard = ({ product, company, onLinkPress }: ProductCardProps) => (
-  <View style={[style.productContainer, base.flex, base.column,base.spaceBetween, base.gap]}>
+
+
+  <View style={[style.productContainer, base.flex, base.column, base.spaceBetween, { gap: 10 }]}>
     <View>
       <Image source={{ uri: product.image }} style={[style.productImage, base.borderRadius]} />
     </View>
     <View style={[base.flex, base.row, base.gap, base.wrap,]}>
       <View>
-        <Image source={{ uri: company.logo }} style={[style.logo, base.borderRadius]} />
+        <Image source={{ uri: company?.logo || 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png' }} style={[style.logo, base.borderRadius]} />
       </View>
       <View>
         <Text>{company.name}</Text>
       </View>
     </View>
 
-    <Text>{product.name}</Text>
-    <View style={[base.flex, base.row, base.spaceBetween, base.wrap,base.maxWidth]}>
+    <Text
+      numberOfLines={3}
+      ellipsizeMode="tail"
+      style={[style.productName]} >
+      {product.name}
+    </Text>
+
+    <View style={[base.flex, base.row, base.spaceBetween, base.wrap, base.maxWidth]}>
       <View>
-      <Text>{product.price.split(' ')[0]}</Text>
+        <Text>{product.price.split(' ')[0]}</Text>
       </View>
-      <TouchableOpacity
+      {/* <TouchableOpacity
         onPress={() => onLinkPress(product.link)}
-        style={[base.alignCenter]}
+        style={[base.alignCenter,]}
       >
-        <Icon name="external-link"size={22}  />
-      </TouchableOpacity>
+        <Icon name="external-link" size={22} />
+      </TouchableOpacity> */}
+      <View style={style.arrowContainer}>
+        <View style={{ backgroundColor: colors.rose, borderRadius: 30 }}>
+          <TouchableOpacity onPress={() => onLinkPress(product.link)} style={[base.alignCenter, style.arrowIcon]} >
+            <Icon name="arrow-up-right" size={24} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
-  </View>
+  </View >
 );
 
 
