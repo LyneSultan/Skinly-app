85c06dc3d0cf7e3da8eb9e7462f091f8405bfccb
LyneSultan <lynesultane@gmail.com>
2025-01-17 23:33:25 +0200

    updated home styling

--- app/(tabs)/HomeScreen/style.ts
+++ app/(tabs)/HomeScreen/style.ts
@@ -4,32 +4,31 @@ import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
   scanContainer: {
-    padding:10,
-    height:45,
+    padding: 10,
+    height: 45,
     backgroundColor: colors.primary,
     ...base.borderRadius,
     ...base.gap,
   },
   productsContainer: {
-    width: "45%",
-    marginBottom:12,
-
+    width: '45%',
+    marginBottom: 12,
   },
   productCard: {
-    gap:7,
-    width: "100%",
+    gap: 7,
+    width: '100%',
   },
   text: {
-    width: "100%",
-    fontSize: 12
+    width: '100%',
+    fontSize: 12,
   },
   productImage: {
-    width: "100%",
+    width: '100%',
     height: 150,
   },
   carousel: {
     width: '100%',
-    height: 230
+    height: 230,
   },
   viewMore: {
     color: colors.primary,
@@ -48,10 +47,12 @@ export const style = StyleSheet.create({
   },
   dotsContainer: {
     marginTop: 10,
+    gap: 10
+    ,
   },
   dot: {
-    width: 10,
-    height: 10,
+    width: 8,
+    height: 8,
     borderRadius: 5,
   },
   activeDot: {
@@ -59,5 +60,5 @@ export const style = StyleSheet.create({
   },
   inactiveDot: {
     backgroundColor: '#ccc',
-  }
+  },
 });
