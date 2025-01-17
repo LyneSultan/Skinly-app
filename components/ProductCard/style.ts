8c588cc941f4e0fa30fd20bc742882cdf298de31
LyneSultan <lynesultane@gmail.com>
2025-01-17 13:04:05 +0200

    updated product card styling

--- components/ProductCard/style.ts
+++ components/ProductCard/style.ts
@@ -4,10 +4,13 @@ import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
   productContainer: {
-    backgroundColor: colors.secondary,
+    backgroundColor: colors.rose,
     width: '47%',
     padding: 10,
+    position: 'relative',
     minHeight: 300,
+    borderWidth: 3, // Optional for clean look
+    borderColor: '#ddd',
     ...base.borderRadius,
   },
   productImage: {
@@ -18,5 +21,27 @@ export const style = StyleSheet.create({
     width: 24,
     height: 24,
     borderRadius: 40,
+  },
+  productName: {
+    marginBottom: 15,
+    lineHeight: 20
+  },
+  arrowIcon: { borderRadius: 20, padding: 5 },
+  arrowContainer: {
+    position: 'absolute', // Places the arrow outside the card
+    top: -20, // Adjust this to move the arrow above the card
+    right: -28, // Adjust this to align the arrow with the card's edge
+    backgroundColor: colors.background, // Circle background
+    borderRadius: 15,
+    padding: 3,
+    width: 55,
+    borderTopColor:'#ddd', // Top border color
+    borderTopWidth: 3,
+    borderLeftWidth: 3, // Ensure other borders are invisible
+    borderLeftColor: '#ddd', // Top border color
+
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
