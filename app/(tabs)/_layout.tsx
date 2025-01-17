8c9cca8bb02efce6df6b0d516e6cd5403328c215
LyneSultan <lynesultane@gmail.com>
2025-01-17 23:32:41 +0200

    updated tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,24 +4,38 @@ import { Tabs } from 'expo-router';
 
 const Layout = () => {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: colors.primary }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary, 
+        tabBarHideOnKeyboard: true, 
+      }} >
       <Tabs.Screen
         name="index"
         options={{ href: null, }} />
 
-      <Tabs.Screen
-        name="ProductSearch/[name]"
-        options={{ href: null, }} />
       <Tabs.Screen
         name="HomeScreen/index"
         options={{
           title: 'Home', tabBarIcon: ({ color }) => <FontAwesome size={24} name="home" color={color} />,
         }}
       />
+
       <Tabs.Screen
-        name="ProductSearch/index"
+        name="ProductSearch/[name]"
         options={{
           title: 'Search', tabBarIcon: ({ color }) => <FontAwesome size={16} name="search" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Ocr/index"
+        options={{
+          title: 'Scan', tabBarIcon: ({ color }) => <FontAwesome size={16} name="camera" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Profile/index"
+        options={{
+          title: 'Profile', tabBarIcon: ({ color }) => <FontAwesome size={16} name="user" color={color} />,
         }}
       />
     </Tabs>
