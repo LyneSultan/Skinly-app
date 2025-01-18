a6e42cb604d07f05a77fe04a5bf747fa94928aec
LyneSultan <lynesultane@gmail.com>
2025-01-19 01:11:49 +0200

    updated skin page logic

--- app/SkinScan/useSkinPageLogic.ts
+++ app/SkinScan/useSkinPageLogic.ts
@@ -1,7 +1,7 @@
 import { PickImage } from '@/hooks/ImagePicker/pickImage';
 import { TakePicture } from '@/hooks/ImagePicker/takePicture';
 import { routes } from '@/routes/server.routes';
-import axios from 'axios';
+import { useRouter } from 'expo-router';
 import { useState } from 'react';
 
 export const useSkinPageLogic = () => {
@@ -10,7 +10,8 @@ export const useSkinPageLogic = () => {
   const { takePicture } = TakePicture();
   const [loading, setLoading] = useState(false);
   const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
-
+  const router = useRouter();
+  const [error, setError] = useState('');
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
   const handlePickImage = async () => {
@@ -19,13 +20,13 @@ export const useSkinPageLogic = () => {
     if (imageUri) {
       sendImageToApi(imageUri);
     }
-  }
+  };
   const handleTakePicture = async () => {
     const imageUri = await takePicture();
     if (imageUri) {
       sendImageToApi(imageUri);
     }
-  }
+  };
 
   const sendImageToApi = async (imageUri: string | undefined) => {
     if (!imageUri) {
@@ -48,35 +49,32 @@ export const useSkinPageLogic = () => {
       } as any);
 
       console.log(apiUrl);
-      const response = await fetch(apiUrl+routes.skinDetection, {
+      const response = await fetch(apiUrl + routes.skinDetection, {
         method: 'POST',
         headers: {
           'Content-Type': 'multipart/form-data',
         },
         body: formData,
       });
+      setError('');
+
 
       const data = await response.json();
       setApiResponse(data);
-      getRecommendation();
+      console.log(data);
+      if (data.class) router.push(`/recommendations?skinType=${data.class}`);
+
+      // getRecommendation();
       setLoading(false);
       console.log('API Response:', data);
     } catch (error) {
+      setApiResponse('');
       setLoading(false);
-      console.error('Error sending image:', error);
+      setError('Please Retry');
+
+      // console.error('Error sending image:', error);
     }
   };
-  const getRecommendation = async() => {
-    try {
-      const recommendation = await axios.get( `${apiUrl}/product/skin/dry`);
-      console.log("data:",recommendation.data);
-      const suggestionsString = recommendation.data.suggestions;
-
-      setSuggestions(suggestionsString);
-    } catch (error) {
-      console.log(error);
-     }
-  }
 
   const imageMap = {
     dry: require('@/assets/images/dry.png'),
@@ -90,10 +88,10 @@ export const useSkinPageLogic = () => {
     handleTakePicture,
     apiResponse,
     suggestions,
-    loading
+    loading,
+    error,
   };
 };
-type Suggestion= {
+type Suggestion = {
   name: string;
-  reason: string;
-}
+};
