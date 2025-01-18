44895204f9ce60ae04dd33a105e9a8df12b8736f
LyneSultan <lynesultane@gmail.com>
2025-01-19 01:15:00 +0200

    added show password in password reset page

--- app/PasswordReset/index.tsx
+++ app/PasswordReset/index.tsx
@@ -2,13 +2,14 @@ import { colors } from "@/colors/colors";
 import ButtonComponent from "@/components/base/Button";
 import { Input } from "@/components/base/Input";
 import { base } from "@/style/base";
+import Checkbox from "expo-checkbox";
 import { Image, Text, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { style } from "./style";
 import useResetLogic from "./useResetLogic";
 
 const PasswordResetPage = () => {
-  const { handleReset, setPassword, setConfirmPassword } = useResetLogic();
+  const { handleReset, setPassword, showPassword, setShowPassword, setConfirmPassword } = useResetLogic();
   return (
     <KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
 
@@ -23,8 +24,15 @@ const PasswordResetPage = () => {
             <Text style={style.subtitle}>Reset Your Password</Text>
 
             <View style={[base.gap]}>
-              <Input text="Password" label="password" onChangeText={(text) => setPassword(text)} />
-              <Input text="confirm password" label="password confirmation" onChangeText={(text) => setConfirmPassword(text)} />
+              <Input text="Password" label="Password" onChangeText={(text) => setPassword(text)} password={!showPassword} />
+              <Input text="Confirm Password" label="Password Confirmation" onChangeText={(text) => setConfirmPassword(text)} password={!showPassword} />
+
+              <View style={style.checkboxContainer}>
+                <Checkbox
+                  value={showPassword}
+                  onValueChange={() => setShowPassword(!showPassword)} />
+                <Text style={style.showPasswordText}>Show Password</Text>
+              </View>
               <ButtonComponent text="Save" onPress={handleReset} />
             </View>
           </View>
