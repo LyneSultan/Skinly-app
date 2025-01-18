b3a697588af2c243ea39bdf00e29183d31b5fe4e
LyneSultan <lynesultane@gmail.com>
2025-01-19 01:14:03 +0200

    added chat bubble to recommendation

--- app/recommendations/index.tsx
+++ app/recommendations/index.tsx
@@ -1,4 +1,5 @@
 import { colors } from '@/colors/colors';
+import ChatBubble from '@/components/Chatbubble';
 import { base } from '@/style/base';
 import React from 'react';
 import { FlatList, Image, ScrollView, Text, View } from 'react-native';
@@ -6,45 +7,42 @@ import { styles } from './style';
 import { useRecommendLogic } from './useRecomendLogic';
 
 const Recommendation = () => {
-  const { skinType,data} = useRecommendLogic();
+  const { skinType, data } = useRecommendLogic();
 
   return (
     <ScrollView style={[{ backgroundColor: colors.background },]}>
 
       <View style={[styles.container, base.default]}>
-
-      <Text style={styles.header}>Your Skin Type: {skinType}</Text>
-
-      {data &&
-        <>
-        <Text style={styles.subHeader}>General Tips For You:</Text>
-
-          <FlatList
-            data={data.tips}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <Text style={styles.tip}>• {item.tip}</Text>}
-            scrollEnabled={false}
-        />
+        <ChatBubble skinType={skinType} />;
 
 
-          <Text style={styles.subHeader}>Recommended Products:</Text>
-          <FlatList
-            data={data.products}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <View style={styles.productContainer}>
-                <Image source={{ uri: item.image_url }} style={styles.productImage} />
-                <View style={styles.productDetails}>
-                  <Text style={styles.productName}>{item.name}</Text>
-                  <Text style={styles.productReason}>{item.reason}</Text>
+        {data &&
+          <>
+            <Text style={styles.subHeader}>General Tips For You:</Text>
+
+            <FlatList
+              data={data.tips}
+              renderItem={({ item }) => <Text style={styles.tip}>• {item}</Text>}
+              scrollEnabled={false}
+            />
+
+
+            <Text style={styles.subHeader}>Recommended Products:</Text>
+            <FlatList
+              data={data.products}
+              renderItem={({ item }) => (
+                <View style={styles.productContainer}>
+                  <Image source={{ uri: item.image_url }} style={styles.productImage} />
+                  <View style={styles.productDetails}>
+                    <Text style={styles.productName}>{item.name}</Text>
+                    <Text style={styles.productReason}>{item.reason}</Text>
+                  </View>
                 </View>
-                </View>
-            )}
-            scrollEnabled={false}
-          />
+              )}
+              scrollEnabled={false} />
 
 
-        </>}
+          </>}
       </View>
 
 
